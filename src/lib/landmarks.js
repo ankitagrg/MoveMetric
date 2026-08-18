ce15ef4578ee34804d5fb8c6e62984dc00869f17
Ankita Gurung <ankitalg18@gmail.com>
2026-08-18 22:19:42 +0545

    feat: define pose landmarks and skeletal connections

--- src/lib/landmarks.js
+++ src/lib/landmarks.js
@@ -3,6 +3,8 @@ export const RIGHT_SHOULDER = 12
 export const LEFT_ELBOW = 13
 export const RIGHT_ELBOW = 14
 export const LEFT_WRIST = 15
+
+
 export const RIGHT_WRIST = 16
 export const LEFT_HIP = 23
 export const RIGHT_HIP = 24
